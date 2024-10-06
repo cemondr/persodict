03849be1bd36424a438c716b7a56f34e784479a7
Cem Onder <cemconder@gmail.com>
2024-10-07 00:45:16 +0200

    Initial Commit

---
+++ persodict/persodictApp.swift
@@ -0,0 +1,17 @@
+//
+//  persodictApp.swift
+//  persodict
+//
+//  Created by Cem Onder on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct persodictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
